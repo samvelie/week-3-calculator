a28f07632b9e88ac0dc43fa7ded2c08cdcdd980e
samvelie <sam.velie@gmail.com>
2017-02-24 19:54:39 -0600

    prev not tested fully, this frankenstein working if a specific order is followed

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,12 @@
 console.log('client.js sourced');
 
 var operation;
+var x; //first number
+var y; //second number
+
+var aNumber = '';
+var bNumber = '';
+var operatorClickedYet = false;
 
 $(document).ready(function(){
   console.log('jquery sourced');
@@ -12,9 +18,20 @@ $(document).ready(function(){
   $('.operator').on('click', function(){
     operation = $(this).attr('id');
     console.log('operation:', operation);
-    $('#display').text();
+    operatorClickedYet = true;
+    $('#display').text($(this).text()); //this would show the operator
+    var currentNumber = $('#display').text();
   });
 
+  $('.number').on('click', function(){
+    if(operatorClickedYet){
+        bNumber += $(this).text();
+        $('#secondNumber').val(bNumber);
+    } else {
+      aNumber += $(this).text();
+      $('#firstNumber').val(aNumber);
+    }
+  })
 
   $('#getResult').on('click', function(){
     console.log('button clicked');
@@ -44,5 +61,14 @@ $(document).ready(function(){
         $('#result').text(data.result);
       }
     })
+  })
+
+  $('#clear').on('click', function(){
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+    $('#result').text('');
+    aNumber = '';
+    bNumber = '';
+    operatorClickedYet = false;
   })
 });
