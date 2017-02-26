52b4f2b71438bbba9278fa7c85e82eed65fcf50a
samvelie <sam.velie@gmail.com>
2017-02-26 17:44:55 -0600

    decimal button causing less trouble

--- server/public/client.js
+++ server/public/client.js
@@ -15,85 +15,97 @@ $(document).ready(function(){
   displayThis(0);  // this 0 helps indicate the calculator is on
   $('.number').on('click', function(){
     var thisNum = $(this).text();  //grabs text of clicked button and stores it
-    if(equalsClicked && result.length > 0){  //when the equals button has retrieved a result, don't want clicking other numbers to affect the result
-    displayThis(thisNum);
-  } else if(operatorClicked){  //when operator has been clicked, this shifts the number buttons to building the y variable
-    y+= thisNum;
-    displayThis(y);
-    console.log('x= ' + x + ' oper= ' + operation + ' y= ' + y);
-  } else {  //this builds the x variable by default
-    x += thisNum;
-    displayThis(x);
-  }
-  numberClicked = true;
-  equalsClicked = false;
-});
-
-$('.operator').on('click', function(){
-  var thisOperator = $(this).attr('id'); //grabs the id which
-
-  if(numberClicked == false && equalsClicked==false && x==''){ //this condition should only work when this operator is clicked first, and not if operator is clicked multiple times
-    x = 0;
-    operation = thisOperator; //I would put this assignment automatically at the button click if I could figure out how to handle the next condtion type
-  } else if(operatorClicked && numberClicked){ //this specific conditional responds to strings of inputs like 22-11+4*4, evaluating the result of the previous two numbers and the chosen operation. I struggled to get this check to work with a server call/response and have this current local fix.
-    var cheating = $('#'+operation).attr('value'); //this obtains the operation symbol that JavaScript can evaluate
-    x = eval(parseFloat(x) + cheating + parseFloat(y)); //this makes x the result of this calculation
-    y = ''; //resets y to nothing to accept further inputs
-    operation = thisOperator; //now resets the operation variable for future operations
-    displayThis(x);
-  } else if(numberClicked){
-    operation = thisOperator;
-  } else if(equalsClicked){
-    operation = thisOperator;
-    y = ''; //this resets y, since the most likely next button clicked will be a number
+    if(equalsClicked &&  x!== ''){  //when the equal button has retrieved a result, clicking a number will be read as the beginning of a new operation
+      x = thisNum;
+      displayThis(x);
+      decimalNotClicked = true;
+    } else if(operatorClicked){  //when operator has been clicked, this shifts the number buttons to building the y variable
+      y+= thisNum;
+      displayThis(y);
+      console.log('x= ' + x + ' oper= ' + operation + ' y= ' + y);
+    } else {  //this builds the x variable by default
+      x += thisNum;
+      displayThis(x);
+    }
+    numberClicked = true;
     equalsClicked = false;
-  }
+  });
 
-  operatorClicked = true;
-  decimalNotClicked = true;
-  numberClicked = false;
-  console.log('x= ' + x + ' oper= ' + operation + ' y= ' + y);
-})
+  $('.operator').on('click', function(){
+    var thisOperator = $(this).attr('id'); //grabs the id which
 
-$('#getResult').on('click', function(){
-  if(x === ''){
-    x = 0;
-  } else if(y ==='' || operation === ''){
-    displayThis(x);
-  } else {
+    if(numberClicked == false && equalsClicked==false && x==''){ //this condition should only work when this operator is clicked first, and not if operator is clicked multiple times
+      x = 0;
+      operation = thisOperator; //I would put this assignment automatically at the button click but I need it to happen after some calculating in the next condition
+    } else if(operatorClicked && numberClicked){ //this specific conditional responds to strings of inputs like 22-11+4*4, evaluating the result of the previous two numbers and the chosen operation. I struggled to get this check to work with a server call/response and have this current local fix.
+      var cheating = $('#'+operation).attr('value'); //this obtains the operation symbol that JavaScript can evaluate
+      x = eval(parseFloat(x) + cheating + parseFloat(y)); //this makes x the result of this calculation
+      operation = thisOperator; //now resets the operation variable for future operations
+      displayThis(x);
+      console.log('cheating');
+    } else if(numberClicked){
+      operation = thisOperator;
+    } else if(equalsClicked){
+      operation = thisOperator;
+      equalsClicked = false;
+      console.log('equals clicked before operator');
+    }
 
-    mathAtServer(x, y, operation);
+    y = ''; //resets y to nothing to prepare for a number being clicked next
+    operatorClicked = true;
+    decimalNotClicked = true;
+    numberClicked = false;
+    console.log('x= ' + x + ' oper= ' + operation + ' y= ' + y);
+  })
+
+  $('#getResult').on('click', function(){
+    if(x === ''){
+      x = 0;
+    } else if(y ==='' || operation === ''){
+      displayThis(x);
+    } else {
+      mathAtServer(x, y, operation);
+    }
 
     equalsClicked = true;
     numberClicked = false;
     operatorClicked = false;
+    decimalNotClicked = false; //making this false here to prevent from modifying the result with decimals
+
+  })
+
+  $('#clear').on('click', function(){
+    displayThis(0);
+    x = '';
+    y = '';
+    operation = '';
+    result = '';
+    numberClicked = false;
+    operatorClicked = false;
+    equalsClicked = false;
     decimalNotClicked = true;
-  }
-})
+  });
 
-$('#clear').on('click', function(){
-  displayThis(0);
-  x = '';
-  y = '';
-  operation = '';
-  result = '';
-  numberClicked = false;
-  operatorClicked = false;
-  equalsClicked = false;
-  decimalNotClicked = true;
-});
-
-$('#decimal').on('click', function(){
-  if (operatorClicked && decimalNotClicked) {
-    y+= '.';
-    displayThis(y);
+  $('#decimal').on('click', function(){
+    if (operatorClicked && decimalNotClicked) {
+      if(y==''){
+        y='0.'
+        displayThis(y);
+      } else {
+        y+= '.';
+        displayThis(y);
+      }
+    } else if(decimalNotClicked){
+      if(x==''){
+        x='0.'
+        displayThis(x);
+      } else {
+        x+= '.';
+        displayThis(x);
+      }
+    }
     decimalNotClicked = false;
-  } else if(decimalNotClicked){
-    x += '.';
-    displayThis(x);
-    decimalNotClicked = false;
-  }
-})
+  })
 
 }); // end document ready
 
